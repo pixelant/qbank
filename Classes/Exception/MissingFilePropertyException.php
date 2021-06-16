727798b00650e64ef16e888aae6c1ba45f6266d5
Mats Svensson <mats@pixelant.se>
2021-06-16 16:24:49 +0200

    [TASK] add missing docblock

--- Classes/Exception/MissingFilePropertyException.php
+++ Classes/Exception/MissingFilePropertyException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Pixelant\Qbank\Exception;
 
+/**
+ * Exception thrown if a file property is missing or doesn't exist.
+ */
 class MissingFilePropertyException extends \RuntimeException
 {
 }
--- Classes/Exception/PersistMetaDataChangesException.php
+++ Classes/Exception/PersistMetaDataChangesException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Pixelant\Qbank\Exception;
 
+/**
+ * Exception thrown if errors were found in DataHandler when MetaData was updated.
+ */
 class PersistMetaDataChangesException extends \RuntimeException
 {
 }
