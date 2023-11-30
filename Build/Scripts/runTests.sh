49cca38f0f9f8e22ac2620e3f57914acd3a0ac1b
Mattias Nilsson <tollepjaer@gmail.com>
2023-11-30 10:23:17 +0100

    [TASK] Additional update for  Build/Scripts/runTests.sh

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -116,7 +116,7 @@ cd ../testing-docker || exit 1
 ROOT_DIR=`realpath ${PWD}/../../`
 TEST_SUITE="unit"
 DBMS="mariadb"
-PHP_VERSION="8.1"
+PHP_VERSION="8.2"
 TYPO3_VERSION="12"
 PHP_XDEBUG_ON=0
 PHP_XDEBUG_PORT=9003
