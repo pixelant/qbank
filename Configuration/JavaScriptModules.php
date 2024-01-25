fa45575685cb41d77d4b01c5553bbc71256e083c
Mattias Nilsson <tollepjaer@gmail.com>
2024-01-25 14:31:19 +0100

    [BUGFIX] Add IRRE support

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -4,6 +4,9 @@ return [
     // required import configurations of other extensions,
     // in case a module imports from another package
     'dependencies' => ['backend'],
+    'tags' => [
+        'backend.form',
+    ],
     'imports' => [
         // recursive definiton, all *.js files in this folder are import-mapped
         // trailing slash is required per importmap-specification
